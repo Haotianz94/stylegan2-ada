36f3bd6fdff70911952d479cece20c181bc3c90b
haotianz <haotianz@stanford.edu>
2020-11-10 20:10:25 -0800

    docker checkpoint

---
+++ scripts/train_djokovic.sh
@@ -0,0 +1,2 @@
+# python train.py --outdir=./training-runs --gpus=2 --data=./datasets/djokovic_fg_009_tf --dry-run
+python train.py --outdir=./training-runs --gpus=2 --data=./datasets/djokovic_fg_009_tf --mirror=1
